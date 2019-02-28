2b38257be5f9533d7ca161bbc1b01d89b70d20d4
Günay YILMAZ <gnyylm@gmail.com>
2019-02-28 17:58:17 +0300

    13

--- galeri/models.py
+++ galeri/models.py
@@ -1,10 +1,17 @@
+import uuid
+import os
 from django.db import models
 from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFill
 
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    return os.path.join('kumes', filename)
+
 class galeri(models.Model):
     title = models.CharField(max_length = 100)
-    image = models.ImageField(upload_to ="kumes")
+    image = models.ImageField(upload_to =get_file_path)
     image_thumbnail = ImageSpecField(source='image',
                                  processors=[ResizeToFill(400, 200)],
                                  format='JPEG',
